4b05bcafa791115e407fa71a2d47b4172392d2dd
mpavithra246 <mpavithra246@gmail.com>
2016-08-09 17:30:09 +0530

    fdghdfh
    gdedrh

---
+++ posnev.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+
+public class posornev {
+
+	public static void main(String[] args) {
+		System.out.println("enter the number");
+		Scanner in=new Scanner(System.in);
+	int n=in.nextInt();
+	if(n>0)
+	{
+		System.out.println("the number is positive");
+	}
+	else
+	{
+System.out.println("the number is negative");
+	}
+
+}
+}
